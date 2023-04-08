b699be925e6d645998dfd96c20de3aa192405e91
Mariel Gauthier-Grégoire <mariel.gauthier.gregoire@gmail.com>
2023-04-08 01:38:38 -0400

    added css for product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,17 @@ import { useCart } from '../contexts/CartContext'
 import formatPrice from '../utilities/formatPrice'
 
 export default function ProductCard({product}) {
+  const { 
+    id, 
+    name, 
+    img, 
+    price,
+    woods,
+    finish,
+    measurements, 
+    description
+  } = product
+
   const {
     getItemQuantity, 
     increaseItemQuantity, 
@@ -9,33 +20,37 @@ export default function ProductCard({product}) {
     removefromCart
   } = useCart()
 
-  const quantity = getItemQuantity(product.id)
+  const quantity = getItemQuantity(id)
 
   return (
     <div className="product-card">
       <div className="card-img">
-        <a href={'/produits/' + product.id}>
-          <img src={product.img} alt={product.name} />
+        <a href={'/produits/' + id}>
+          <img src={img} alt={name} />
         </a>
       </div>
       <div className="card-body">
         <div className="card-title">
-          <a href={'/produits/' + product.id}>
-            <span className="hover-highlight">{product.name}</span>
-            <span className="price hover-highlight">{formatPrice(product.price, true)}</span>
-          </a>
+          <span className="hover-highlight">
+            <a href={'/produits/' + id}>{name}</a>
+          </span>
+          <span className="price hover-highlight">{formatPrice(price, true)}</span>
         </div>
+        <div>{woods}</div>
+        <div>{finish}</div>
+        <div>{measurements}</div>
+        <div>{description}</div>
         <div className="card-actions">
           {quantity === 0 ? (
-            <button onClick={() => increaseItemQuantity(product.id)}>Ajouter au panier</button>
+            <button className='button-89' onClick={() => increaseItemQuantity(id)}>Ajouter au panier</button>
           ) : (
             <div className="qty-group">
               <div className="qty-handler">
-                <button onClick={() => decreaseItemQuantity(product.id)}>-</button>
-                <div className="number">{quantity}</div>
-                <button onClick={() => increaseItemQuantity(product.id)}>+</button>
+                <button onClick={() => decreaseItemQuantity(id)}>-</button>
+                <div className="qty">{quantity}</div>
+                <button onClick={() => increaseItemQuantity(id)}>+</button>
               </div>
-              <button onClick={() => removefromCart(product.id)}>Retirer</button>
+              <button onClick={() => removefromCart(id)}>Retirer</button>
             </div>
           )}
         </div>
